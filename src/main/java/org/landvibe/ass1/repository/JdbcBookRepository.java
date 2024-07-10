f75b4438bbc569eb90b96edc8e77096e309650ca
임주민 <limj1213@gmail.com>
2024-07-10 20:41:22 +0900

    feat(repository) : implement repository interface with jdbc

---
+++ src/main/java/org/landvibe/ass1/repository/JdbcBookRepository.java
@@ -0,0 +1,146 @@
+package org.landvibe.ass1.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.landvibe.ass1.domain.Book;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class JdbcBookRepository implements BookRepository {
+
+    private final DataSource dataSource;
+
+    @Override
+    public Book save(Book book) {
+        String sql = "insert into books(title) values(?)";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, book.getTitle());
+
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                book.setId(resultSet.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return book;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    @Override
+    public Optional<Book> findBookById(Long id) {
+        String sql = "select * from books where id = ?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Book book = new Book();
+                book.setTitle(resultSet.getString("title"));
+                book.setId(resultSet.getLong("id"));
+                return Optional.of(book);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        String sql = "select * from books";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            resultSet = preparedStatement.executeQuery();
+
+            List<Book> books = new ArrayList<>();
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setTitle(resultSet.getString("title"));
+                book.setId(resultSet.getLong("id"));
+                books.add(book);
+            }
+            return books;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection connection,
+                       PreparedStatement preparedStatement,
+                       ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) {
+                close(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+}
