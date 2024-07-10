db44f674e7accda64816afee9c7a890072e9bab1
임주민 <limj1213@gmail.com>
2024-07-10 20:40:01 +0900

    feat(service) : add logic for book data creation and search

---
+++ src/main/java/org/landvibe/ass1/service/BookService.java
@@ -0,0 +1,27 @@
+package org.landvibe.ass1.service;
+
+import lombok.RequiredArgsConstructor;
+import org.landvibe.ass1.domain.Book;
+import org.landvibe.ass1.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public Book insertBook(String title) {
+        return bookRepository.save(new Book(title));
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findBookById(id).orElseThrow();
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAllBooks();
+    }
+}
