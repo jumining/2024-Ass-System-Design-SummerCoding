738e27c9ea2b4ce7eb6e49e4f3104c36fa91674b
임주민 <limj1213@gmail.com>
2024-07-10 20:40:36 +0900

    feat(repository) : add interface about repository for book

---
+++ src/main/java/org/landvibe/ass1/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package org.landvibe.ass1.repository;
+
+import org.landvibe.ass1.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository {
+
+    Book save(Book book);
+    Optional<Book> findBookById(Long id);
+    List<Book> findAllBooks();
+}
