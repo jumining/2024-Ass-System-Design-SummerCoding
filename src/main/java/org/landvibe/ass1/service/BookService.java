6a4feb63803f019d129093b547a56d82f6ba767a
임주민 <limj1213@gmail.com>
2024-07-25 11:20:16 +0900

    feat(service) : apply annotation about caching to service method

--- src/main/java/org/landvibe/ass1/service/BookService.java
+++ src/main/java/org/landvibe/ass1/service/BookService.java
@@ -1,7 +1,10 @@
 package org.landvibe.ass1.service;
 
 import lombok.RequiredArgsConstructor;
+import org.landvibe.ass1.cache.annotation.CacheInLandvibe;
+import org.landvibe.ass1.cache.annotation.CacheOutLandvibe;
 import org.landvibe.ass1.domain.Book;
+import org.landvibe.ass1.cache.annotation.CachingLandvibe;
 import org.landvibe.ass1.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,12 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    @CacheInLandvibe(tableName = "bookCache", key = "#id")
     public Book insertBook(String title) {
         return bookRepository.save(new Book(title));
     }
 
+    @CachingLandvibe(tableName = "bookCache", key = "#id")
     public Book getBookById(Long id) {
         return bookRepository.findBookById(id).orElseThrow();
     }
@@ -24,7 +29,14 @@ public class BookService {
     public List<Book> getAllBooks() {
         return bookRepository.findAllBooks();
     }
+
+    @CacheOutLandvibe(tableName = "bookCache", key = "#id")
     public Long deleteBookById(Long id) {
         return bookRepository.deleteBookById(id);
+    }
+
+    @CacheOutLandvibe(tableName = "bookCache", key = "#id")
+    public void deleteBookFromCacheById(Long id) {
+        System.out.println("delete from cache");
     }
 }
